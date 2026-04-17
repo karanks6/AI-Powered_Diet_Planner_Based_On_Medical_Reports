9b67a7f90fc4e00ff4a624d9385a964d64bd005a
karanks6 <suvarnakaran77@gmail.com>
2026-04-17 14:06:35 +0530

    initial commit

---
+++ static - Copy/js/main.js
@@ -0,0 +1,14 @@
+AOS.init({
+  duration: 800,
+  easing: 'ease-in-out',
+  once: true
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
