bd698372f2da9b0331bd9886cdd8cba785332f22
karanks6 <suvarnakaran77@gmail.com>
2026-05-12 01:17:05 +0530

    code refinement

--- app.py
+++ app.py
@@ -133,7 +133,6 @@ def results():
         plan_type=profile.get("plan_type", "daily"),
     )
 
-
 # ---------------------- DOWNLOAD PLAN AS PDF ---------------------- #
 @app.route("/download_plan")
 def download_plan():
